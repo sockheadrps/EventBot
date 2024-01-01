4fcc6aaf67815a3470bf7cb95c6d2bd78a95831d
sockheadrps <r.p.skiles@gmail.com>
2023-12-31 22:29:31 -0600

    working on windows run script

---
+++ linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export TOKEN=""
+export EVENT_CHANNEL=""
+source .venv/bin/activate
+python3.11 main.py
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export TOKEN="MTE5MDE2NDExNzY5MzE1NzQwNg.Gr5B8E.iFNWmMEfesxaQ2h97XB-rejwdtU4ejAufW5pls"
-export EVENT_CHANNEL="1190731011378651176"
-source .venv/bin/activate
-python3.11 main.py
